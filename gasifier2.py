1ea4b7662386346d081049ae76577ef3baa49424
Nícolas Anése <nicolasanese19@gmail.com>
2022-05-22 14:37:41 -0300

    Updated changelog

--- gasifier2.py
+++ gasifier2.py
@@ -228,7 +228,7 @@ def isotCogasification(fuel1, fuel2, mass=1.0, blend=0.5, moist=(0.0,0.0), T=127
 
     return outlet, inlet, fuelBlend
 
-def gasifier(fuelID, fuelMass=1.0, moist=0.0, T=1273.15, P=ct.one_atm, 
+def gasifier(fuelID, mass=1.0, moist=0.0, T=1273.15, P=ct.one_atm, 
                 air=0.5, stm=0.0, airType='ER', stmType='SR', isot=True,
                 species=['C(gr)','N2','O2','H2','CO','CH4','CO2','H2O']):
     '''
@@ -238,7 +238,7 @@ def gasifier(fuelID, fuelMass=1.0, moist=0.0, T=1273.15, P=ct.one_atm,
     ----------
     fuelID : str
         ID of fuel as given by the database (fuels.csv)
-    fuelMass : float
+    mass : float
         The fuel mass [kg] (default: 1.0 kg)
     moist : float
         Moisture content of fuel [kg/kg] in dry basis (default value is zero)
@@ -267,7 +267,7 @@ def gasifier(fuelID, fuelMass=1.0, moist=0.0, T=1273.15, P=ct.one_atm,
     '''
     if isot:
         # Isothermal gasification
-        outlet, inlet, fuelMix = isotGasification(fuelID, fuelMass, moist, T, P, air, stm, airType, stmType)
+        outlet, inlet, fuelMix = isotGasification(fuelID, mass, moist, T, P, air, stm, airType, stmType)
     else:
         raise NotImplementedError('Non-isothermal gasification not yet implemented.')
 
@@ -300,10 +300,10 @@ def gasifier(fuelID, fuelMass=1.0, moist=0.0, T=1273.15, P=ct.one_atm,
 
     report['FuelID'] = fuelID
     report['Fuel'] = fu.fuels.loc[fuelID]['Description']
-    report['Mass [kg]'] = fuelMass
-    report['Moisture [kg/kg]'] = moist
-    report['T [°C]'] = T - 273.15
-    report['P [atm]'] = P/ct.one_atm
+    report['Mass'] = mass
+    report['Moisture'] = moist
+    report['T'] = T - 273.15
+    report['P'] = P/ct.one_atm
     report['ER'] = ER
     report['SR'] = SR
 
@@ -316,12 +316,12 @@ def gasifier(fuelID, fuelMass=1.0, moist=0.0, T=1273.15, P=ct.one_atm,
         report[s] = fracs[i]
 
     report['H2/CO'] = op.H2CO(outlet)
-    report['CC [%]'] = op.carbonConv(outlet, inlet)*100
-    report['Y [Nm³/kg]'] = op.gasYield(outlet, basis='vol')/fuelMass
-    report['HHV [MJ/kg]'] = op.syngasHHV(outlet, basis='fuel mass', fuelMass=fuelMass)
+    report['CC'] = op.carbonConv(outlet, inlet)*100
+    report['Y'] = op.gasYield(outlet, basis='vol')/mass
+    report['HHV'] = op.syngasHHV(outlet, basis='fuel mass', fuelMass=mass)
 
     fuelLHV = fu.HV(fuelID, type='LHV', moist=moist)
-    report['CGE [%]'] = op.coldGasEff(outlet, fuelLHV, moist=moist)*100  
+    report['CGE'] = op.coldGasEff(outlet, fuelLHV, moist=moist)*100  
 
     return report
 
@@ -401,14 +401,14 @@ def cogasifier(fuel1, fuel2, mass=1.0, blend=0.5, moist=(0.0,0.0), T=1273.15,
 
     report['Fuel #1'] = fu.fuels.loc[fuel1]['Description']
     report['Fuel #2'] = fu.fuels.loc[fuel2]['Description']
-    report['Total mass [kg]'] = mass
-    report['Blend [%]'] = blend*100
+    report['Mass'] = mass
+    report['Blend'] = blend*100
 
     blendMoist = moist[0]*(1-blend) + moist[1]*blend
-    report['Moisture [kg/kg]'] = blendMoist
+    report['Moisture'] = blendMoist
 
-    report['T [°C]'] = T - 273.15
-    report['P [atm]'] = P/ct.one_atm
+    report['T'] = T - 273.15
+    report['P'] = P/ct.one_atm
     report['ER'] = ER
     report['SR'] = SR
 
@@ -421,55 +421,55 @@ def cogasifier(fuel1, fuel2, mass=1.0, blend=0.5, moist=(0.0,0.0), T=1273.15,
         report[s] = fracs[i]*100
 
     report['H2/CO'] = op.H2CO(outlet)
-    report['CC [%]'] = op.carbonConv(outlet, inlet)*100
-    report['Y [Nm³/kg]'] = op.gasYield(outlet, basis='vol')/mass
-    report['HHV [MJ/kg]'] = op.syngasHHV(outlet, basis='fuel mass', fuelMass=mass)
+    report['CC'] = op.carbonConv(outlet, inlet)*100
+    report['Y'] = op.gasYield(outlet, basis='vol')/mass
+    report['HHV'] = op.syngasHHV(outlet, basis='fuel mass', fuelMass=mass)
 
     fuel1LHV = fu.HV(fuel1, type='LHV', moist=moist[0])
     fuel2LHV = fu.HV(fuel2, type='LHV', moist=moist[1])
     blendLHV = fuel1LHV*(1-blend) + fuel2LHV*blend
-    report['% CGE'] = op.coldGasEff(outlet, blendLHV, moist=blendMoist)*100 
+    report['CGE'] = op.coldGasEff(outlet, blendLHV, moist=blendMoist)*100 
 
     return report
 
-def oneToOne(conditions):
-    '''
-    Given a list of multiple items, some of them lists, returns a list of conditions matching 1 to 1 each item.
+# def oneToOne(conditions):
+#     '''
+#     Given a list of multiple items, some of them lists, returns a list of conditions matching 1 to 1 each item.
 
-    Parameters
-    ----------
-    conditions : list
-        List of conditions, where each item can be a list or a single value. All lists must have the same length.
+#     Parameters
+#     ----------
+#     conditions : list
+#         List of conditions, where each item can be a list or a single value. All lists must have the same length.
     
-    Returns
-    -------
-    oneToOne : list
-        List of conditions matching 1 to 1 each item.
-    '''
+#     Returns
+#     -------
+#     oneToOne : list
+#         List of conditions matching 1 to 1 each item.
+#     '''
 
-    # Find first item that is of type list
-    for c in conditions:
-        if isinstance(c, list):
-            Len = len(c)
+#     # Find first item that is of type list
+#     for c in conditions:
+#         if isinstance(c, list):
+#             Len = len(c)
     
-    listIndex = []
-    # Now, check if all list items are of the same length
-    for c in conditions:
-        if isinstance(c, list):
-            if len(c) != Len:
-                raise ValueError('All lists must have the same length.')
+#     listIndex = []
+#     # Now, check if all list items are of the same length
+#     for c in conditions:
+#         if isinstance(c, list):
+#             if len(c) != Len:
+#                 raise ValueError('All lists must have the same length.')
 
-    oneToOne = []
-    for j in range(Len):
-        item = []
-        for c in conditions:
-            if isinstance(c, list):
-                item.append(c[j])
-            else:
-                item.append(c)
-        oneToOne.append(item)
+#     oneToOne = []
+#     for j in range(Len):
+#         item = []
+#         for c in conditions:
+#             if isinstance(c, list):
+#                 item.append(c[j])
+#             else:
+#                 item.append(c)
+#         oneToOne.append(item)
 
-    return oneToOne
+#     return oneToOne
         
 # def combinatorial(conditions):
 #     '''
